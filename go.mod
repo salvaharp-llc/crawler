4be139f07449142d1f3a76464271f0ab08f04cd2
salvaharp-llc <salvaharp@gmail.com>
2025-12-12 13:29:57 +0100

    add normalizeURL

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/salvaharp-llc/crawler
+
+go 1.25.0
---
+++ normalize_url.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"fmt"
+	"net/url"
+	"strings"
+)
+
+func normalizeURL(rawURL string) (string, error) {
+	parsedURL, err := url.Parse(rawURL)
+	if err != nil {
+		return "", fmt.Errorf("couldn't normalize URL: %w", err)
+	}
+
+	fullPath := parsedURL.Host + parsedURL.Path
+
+	fullPath = strings.TrimSuffix(fullPath, "/")
+
+	fullPath = strings.ToLower(fullPath)
+
+	return fullPath, nil
+}
